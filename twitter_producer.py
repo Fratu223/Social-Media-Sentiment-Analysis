1e7d7f1f08b661190906ba0b62bc3727cc988126
Fratu223 <mihaifratu78@gmail.com>
2025-07-02 14:22:28 +0200

    Setup Logging

--- twitter_producer.py
+++ twitter_producer.py
@@ -1,3 +1,4 @@
+# Import needed libraries
 import json
 import logging
 import os
@@ -9,3 +10,18 @@ from typing import Dict, Any, Optional
 import requests
 from kafka import KafkaProducer
 from kafka.errors import KafkaError
+
+class TwitterKafkaProducer:
+
+
+    def __init__(self, bearer_token: str, kafka_config: Dict[str, Any], topic: str):
+        self.bearer_token = bearer_token
+        self.topic = topic
+        self.running = True
+
+        # Setup logging
+        logging.basicConfig(
+            level=logging.INFO,
+            format='%(asctime)s - %(levelname)s - %(message)s'
+        )
+        self.logger = logging.getLogger(__name__)
