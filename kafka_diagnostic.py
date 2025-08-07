915737f1f50008149369d061772661e8448faf35
Fratu223 <mihaifratu78@gmail.com>
2025-08-07 12:30:49 +0200

    Import needed libraries

--- kafka_diagnostic.py
+++ kafka_diagnostic.py
@@ -1,1 +1,10 @@
 # flake8: noqa: F401
+# Kafka diagnostic script to help troubleshoot connection issues
+
+# Import needed libraries
+import os
+from dotenv import load_dotenv
+from kafka import KafkaConsumer, KafkaProducer
+from kafka.errors import KafkaError
+import json
+import time
